eaa71e9164c335bf38b74243b67e9fdfe1f8b055
su-kun1899 <higedrum.coz@gmail.com>
2017-07-01 15:56:26 +0900

    Add check expression

--- src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyLimitedExpressionParser.java
+++ src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyLimitedExpressionParser.java
@@ -1,5 +1,6 @@
 package com.github.kmizu.parser_hands_on.my_parser;
 
+import com.github.kmizu.parser_hands_on.ParseFailure;
 import com.github.kmizu.parser_hands_on.limited_expression.AbstractLimitedExpressionParser;
 import com.github.kmizu.parser_hands_on.limited_expression.LimitedExpressionNode;
 
@@ -9,6 +10,22 @@ import com.github.kmizu.parser_hands_on.limited_expression.LimitedExpressionNode
 public class MyLimitedExpressionParser extends AbstractLimitedExpressionParser {
     @Override
     public LimitedExpressionNode parse(String input) {
+        int left = left(input);
+
         return null;
+    }
+
+    private int left(String input) {
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char target = input.charAt(i);
+            if (target == '+' || target == '-' || target == '*' || target == '/') {
+                return result;
+            }
+
+            result = result * 10 + (target - '0');
+        }
+
+        throw new ParseFailure("input is not limited expression node. input:" + input);
     }
 }
