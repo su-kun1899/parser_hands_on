105414ec4b9fb4f84b9189e20ef6a80091c10cb5
su-kun1899 <higedrum.coz@gmail.com>
2017-07-01 14:22:42 +0900

    Add check first digit zero

--- src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyIntegerParser.java
+++ src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyIntegerParser.java
@@ -3,15 +3,21 @@ package com.github.kmizu.parser_hands_on.my_parser;
 import com.github.kmizu.parser_hands_on.ParseFailure;
 import com.github.kmizu.parser_hands_on.integer.AbstractIntegerParser;
 
+import java.util.Arrays;
+
 /**
  * @author su-kun1899
  */
 public class MyIntegerParser extends AbstractIntegerParser {
     @Override
     public Integer parse(String input) {
-        char first = input.charAt(0);
-        if (!isDigit(first)) {
-            throw new ParseFailure("first character is not digit");
+        if (input.startsWith("0")) {
+            throw new ParseFailure("input starts with zero digit");
+        }
+
+        boolean containsNotDigit = !(input.chars().allMatch(value -> isDigit((char) value)));
+        if (containsNotDigit) {
+            throw new ParseFailure("input is not digit");
         }
         return null;
     }
