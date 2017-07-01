142b4317e9d7032a93f719c5fb1c047a5018148b
su-kun1899 <higedrum.coz@gmail.com>
2017-07-01 16:42:30 +0900

    Enable to work

--- src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyLimitedExpressionParser.java
+++ src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyLimitedExpressionParser.java
@@ -4,6 +4,8 @@ import com.github.kmizu.parser_hands_on.ParseFailure;
 import com.github.kmizu.parser_hands_on.limited_expression.AbstractLimitedExpressionParser;
 import com.github.kmizu.parser_hands_on.limited_expression.LimitedExpressionNode;
 
+import static com.github.kmizu.parser_hands_on.limited_expression.LimitedExpressionNode.*;
+
 /**
  * @author su-kun1899
  */
@@ -12,19 +14,33 @@ public class MyLimitedExpressionParser extends AbstractLimitedExpressionParser {
     public LimitedExpressionNode parse(String input) {
         int left = integer(input, 0);
 
+        char operand = 0;
         for (int i = 0; i < input.length(); i++) {
             char target = input.charAt(i);
             if (isOperand(target)) {
+                operand = target;
                 position = i;
                 break;
             }
         }
         if (position != 0) {
             int right = integer(input, position + 1);
-            return new LimitedExpressionNode.Addition(left, right);
+
+            switch (operand) {
+                case '+':
+                    return add(left, right);
+                case '-':
+                    return sub(left, right);
+                case '*':
+                    return mul(left, right);
+                case '/':
+                    return div(left, right);
+                default:
+                    throw new ParseFailure("unknown operand. operand: " + operand);
+            }
         }
 
-        return new LimitedExpressionNode.ValueNode(left);
+        return v(left);
     }
 
     private int integer(String input, int position) {
