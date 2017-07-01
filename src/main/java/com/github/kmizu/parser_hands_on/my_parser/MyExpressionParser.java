807fe8bfaac5fce93c0cb8e673346762f2e25819
su-kun1899 <higedrum.coz@gmail.com>
2017-07-01 18:54:27 +0900

    Refactor

--- src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyExpressionParser.java
+++ src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyExpressionParser.java
@@ -25,6 +25,10 @@ public class MyExpressionParser extends AbstractExpressionParser {
         return ('0' <= c) && (c <= '9');
     }
 
+    private boolean isOperand(char c) {
+        return c == '+' || c == '-' || c == '*' || c == '/';
+    }
+
     public ExpressionNode integer(String input) {
         int result = 0;
         while (position < input.length()) {
@@ -40,26 +44,31 @@ public class MyExpressionParser extends AbstractExpressionParser {
     }
 
     private ExpressionNode expression(String input) {
-        ExpressionNode left = integer(input);
-        if (position < input.length()) {
+        ExpressionNode result = integer(input);
+        while (position < input.length()) {
             char ch = input.charAt(position);
-            switch (ch) {
-                case '+':
-                    position++;
-                    return left.add(integer(input));
-                case '-':
-                    position++;
-                    return left.subtract(integer(input));
-                case '*':
-                    position++;
-                    return left.multiply(integer(input));
-                case '/':
-                    position++;
-                    return left.divide(integer(input));
-                default:
-                    throw new ParseFailure("input can't contain not integer. input: " + input);
+            if (isOperand(ch)) {
+                position++;
+                switch (ch) {
+                    case '+':
+                        result = result.add(integer(input));
+                        break;
+                    case '-':
+                        result = result.subtract(integer(input));
+                        break;
+                    case '*':
+                        result = result.multiply(integer(input));
+                        break;
+                    case '/':
+                        result = result.divide(integer(input));
+                        break;
+                    default:
+                        throw new ParseFailure("Unsupported operand: " + ch);
+                }
+                continue;
             }
+            throw new ParseFailure("input can't contain not integer. input: " + input);
         }
-        return left;
+        return result;
     }
 }
