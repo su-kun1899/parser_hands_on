c15fc7d9f2c0164d6113558f8729b893e3255660
su-kun1899 <higedrum.coz@gmail.com>
2017-07-01 15:24:47 +0900

    Refactor

--- src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyIntegerParser.java
+++ src/main/java/com/github/kmizu/parser_hands_on/my_parser/MyIntegerParser.java
@@ -9,16 +9,22 @@ import com.github.kmizu.parser_hands_on.integer.AbstractIntegerParser;
 public class MyIntegerParser extends AbstractIntegerParser {
     @Override
     public Integer parse(String input) {
-        if (!input.equals("0") && input.startsWith("0")) {
-            throw new ParseFailure("input starts with zero digit");
+        if (input.startsWith("0")) {
+            if (input.length() != 1) {
+                throw new ParseFailure("input starts with zero digit");
+            }
         }
 
-        boolean containsNotDigit = !(input.chars().allMatch(value -> isDigit((char) value)));
-        if (containsNotDigit) {
-            throw new ParseFailure("input is not digit");
+        if (input.chars().anyMatch(value -> !isDigit((char) value))) {
+            throw new ParseFailure("input is not integer. input: " + input);
         }
 
-        return Integer.valueOf(input);
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            result = result * 10 + (input.charAt(i) - '0');
+        }
+
+        return result;
     }
 
     private boolean isDigit(char c) {
